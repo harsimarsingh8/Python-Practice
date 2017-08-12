d4cd7e930c10b10260bc94804bc1ffc32fc0548e
Harsimar Singh <harsimar.singh08@gmail.com>
2017-08-12 20:20:24 +0530

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+##############num=input("enter a number")
+##############reverse=0
+##############old=num
+##############while(num!=0):
+##############    num1=num%10
+##############    reverse=reverse*10+num1
+##############    num=num/10
+##############
+##############if reverse==old:
+##############    print("it is a palindrome number")
+##############else:
+##############    print("it is not a palindrome")
+##############
+##############    
+############///////////////////////////////////
+
