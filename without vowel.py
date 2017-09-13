7f4531dbef172724c9516c392fa7317f3f3d1859
Harsimar Singh <harsimar.singh08@gmail.com>
2017-09-13 18:39:11 +0530

    no vowel

---
+++ without vowel.py
@@ -0,0 +1,6 @@
+s='harsimar'            #can use "   "
+for i in s:
+    if(i=='a'or i=='e'or i=='i'or i=='o'or i=='u'):
+        continue  #or we can use b=0 or any command
+    else:
+        print i,
