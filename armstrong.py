045b8fdcfab716c00114c8c3aeee4b6dcde408ff
Harsimar Singh <harsimar.singh08@gmail.com>
2017-10-20 17:04:36 +0530

    armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -3,7 +3,7 @@ n=input("enter the number of digits\n")
 num=input("enter the number\n")
 old=num 
 while(num!=0):
-    num1=num%10
+    num1=num%10 
     sum=sum+(num1**n)
     num=num/10
 if(old==sum): 
